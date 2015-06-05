50b44a23e9a5bbd8b1abc82118c705d1009870d0
Assela Pathirana <assela@pathirana.net>
2015-06-05 19:54:48 +0200

    custom migrate method for cms app added to settings

--- djcmsmysite/settings.py
+++ djcmsmysite/settings.py
@@ -237,4 +237,5 @@ MIGRATION_MODULES = {
     'djangocms_picture': 'djangocms_picture.migrations_django',
     'djangocms_teaser': 'djangocms_teaser.migrations_django',
     'djangocms_video': 'djangocms_video.migrations_django'
+    'cms': 'cms.migrations_django'
 }
